18af4e5120b152dc32300b05a0f752593331ff31
Karol Wypchlo <kwypchlo@gmail.com>
2022-06-29 16:26:35 +0200

    test cases updates

--- nginx/libs/skynet/skylink.spec.lua
+++ nginx/libs/skynet/skylink.spec.lua
@@ -1,61 +1,88 @@
 local skynet_skylink = require("skynet.skylink")
 
-local base32 = "0404dsjvti046fsua4ktor9grrpe76erq9jot9cvopbhsvsu76r4r30"
-local base64 = "AQBG8n_sgEM_nlEp3G0w3vLjmdvSZ46ln8ZXHn-eObZNjA"
+local skylink_base64 = "EADi4QZWt87sSDCSjVTcmyI5tE_YAsuC90BcCi_jEmG5NA"
+local resolver_skylink_base32 = "0404dsjvti046fsua4ktor9grrpe76erq9jot9cvopbhsvsu76r4r30"
+local resolver_skylink_base64 = "AQBG8n_sgEM_nlEp3G0w3vLjmdvSZ46ln8ZXHn-eObZNjA"
 
 describe("parse", function()
    it("should return unchanged base64 skylink", function()
-      assert.is.same(skynet_skylink.parse(base64), base64)
+      assert.is.same(resolver_skylink_base64, skynet_skylink.parse(resolver_skylink_base64))
    end)
 
    it("should transform base32 skylink into base64", function()
-      assert.is.same(skynet_skylink.parse(base32), base64)
+      assert.is.same(resolver_skylink_base64, skynet_skylink.parse(resolver_skylink_base32))
    end)
 end)
 
 describe("validate", function()
    it("should return nil for valid base32 skylink", function()
-      assert.is_nil(skynet_skylink.validate(base32))
+      assert.is_nil(skynet_skylink.validate(resolver_skylink_base32))
    end)
 
    it("should return nil for valid base64 skylink", function()
-      assert.is_nil(skynet_skylink.validate(base64))
+      assert.is_nil(skynet_skylink.validate(resolver_skylink_base64))
+   end)
+
+   it("should return size error when skylink is empty", function()
+      assert.is_equal("Skylink has incorrect size", skynet_skylink.validate(""))
    end)
 
    it("should return size error when skylink does not match neither base32 nor base64 size", function()
-      assert.is_equal(skynet_skylink.validate(""), "Skylink has incorrect size")
-      assert.is_equal(skynet_skylink.validate(base32 .. "a"), "Skylink has incorrect size")
-      assert.is_equal(skynet_skylink.validate(base64 .. "a"), "Skylink has incorrect size")
+      assert.is_equal("Skylink has incorrect size", skynet_skylink.validate(resolver_skylink_base32 .. "a"))
+      assert.is_equal("Skylink has incorrect size", skynet_skylink.validate(resolver_skylink_base64 .. "a"))
    end)
 
-   it("should return position of a bad character in base32 encoded skylink", function()
-      -- invalid first char * (asterisk)
-      assert.is_equal(skynet_skylink.validate("*404dsjvti046fsua4ktor9grrpe76erq9jot9cvopbhsvsu76r4r30"), "Base32 encoded skylink contains invalid char at position 1")
+   describe("with a bad character in base32 encoded skylink", function()
+      it("should return position when first character is bad", function()
+         local error = "Base32 encoded skylink contains invalid char at position 1"
 
-      -- invalid last char x
-      assert.is_equal(skynet_skylink.validate("0404dsjvti046fsua4ktor9grrpe76erq9jot9cvopbhsvsu76r4r3x"), "Base32 encoded skylink contains invalid char at position 55")
+         -- invalid first char * (asterisk) at position 1
+         assert.is_equal(error, skynet_skylink.validate("*404dsjvti046fsua4ktor9grrpe76erq9jot9cvopbhsvsu76r4r30"))
+      end)
 
-      -- invalid char . (dot)
-      assert.is_equal(skynet_skylink.validate("0404dsjvti046fsua4ktor9gr.pe76erq9jot9cvopbhsvsu76r4r30"), "Base32 encoded skylink contains invalid char at position 26")
+      it("should return position when last character is bad", function()
+         local error = "Base32 encoded skylink contains invalid char at position 55"
+
+         -- invalid last char x at position 55
+         assert.is_equal(error, skynet_skylink.validate("0404dsjvti046fsua4ktor9grrpe76erq9jot9cvopbhsvsu76r4r3x"))
+      end)
+
+      it("should return position when middle character is bad", function()
+         local error = "Base32 encoded skylink contains invalid char at position 26"
+
+         -- invalid char . (dot) at position 26
+         assert.is_equal(error, skynet_skylink.validate("0404dsjvti046fsua4ktor9gr.pe76erq9jot9cvopbhsvsu76r4r30"))
+      end)
    end)
 
-   it("should return position of a bad character in base64 encoded skylink", function()
-      -- invalid first char * (asterisk)
-      assert.is_equal(skynet_skylink.validate("*QBG8n_sgEM_nlEp3G0w3vLjmdvSZ46ln8ZXHn-eObZNjA"), "Base64 encoded skylink contains invalid char at position 1")
+   describe("with a bad character in base64 encoded skylink", function()
+      it("should return position when first character is bad", function()
+         local error = "Base64 encoded skylink contains invalid char at position 1"
 
-      -- invalid last char / (forward slash)
-      assert.is_equal(skynet_skylink.validate("AQBG8n_sgEM_nlEp3G0w3vLjmdvSZ46ln8ZXHn-eObZNj/"), "Base64 encoded skylink contains invalid char at position 46")
+         -- invalid first char * (asterisk) at position 1
+         assert.is_equal(error, skynet_skylink.validate("*QBG8n_sgEM_nlEp3G0w3vLjmdvSZ46ln8ZXHn-eObZNjA"))
+      end)
 
-      -- invalid char . (dot)
-      assert.is_equal(skynet_skylink.validate("AQBG8n_sgEM_nlEp3G0w3vLjm.vSZ46ln8ZXHn-eObZNjA"), "Base64 encoded skylink contains invalid char at position 26")
+      it("should return position when last character is bad", function()
+         local error = "Base64 encoded skylink contains invalid char at position 46"
+
+         -- invalid last char / (forward slash) at position 46
+         assert.is_equal(error, skynet_skylink.validate("AQBG8n_sgEM_nlEp3G0w3vLjmdvSZ46ln8ZXHn-eObZNj/"))
+      end)
+
+      it("should return position when middle character is bad", function()
+         local error = "Base64 encoded skylink contains invalid char at position 26"
+
+         -- invalid char . (dot) at position 26
+         assert.is_equal(error, skynet_skylink.validate("AQBG8n_sgEM_nlEp3G0w3vLjm.vSZ46ln8ZXHn-eObZNjA"))
+      end)
    end)
 end)
 
 describe("hash", function()
-   local base64 = "EADi4QZWt87sSDCSjVTcmyI5tE_YAsuC90BcCi_jEmG5NA"
-   local hash = "6cfb9996ad74e5614bbb8e7228e72f1c1bc14dd9ce8a83b3ccabdb6d8d70f330"
-
    it("should hash skylink", function()
-      assert.is.same(hash, skynet_skylink.hash(base64))
+      local hash = "6cfb9996ad74e5614bbb8e7228e72f1c1bc14dd9ce8a83b3ccabdb6d8d70f330"
+
+      assert.is.same(hash, skynet_skylink.hash(skylink_base64))
    end)
 end)
